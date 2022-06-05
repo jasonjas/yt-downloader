4ba7b1db27ad37890c7f95af8e8b9f98375abc3f
jason <jasonjas@gmail.com>
2022-06-05 08:17:32 -0500

    Configure manage_defaults to work correctly using defaults file

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import main_window, manage_files
 
 def main():
-    #mf = manage_files.DefaultLocations()
-    #mf.check_defaults_file()
+    mf = manage_files.DefaultLocations()
+    mf.check_defaults_file()
     main_window.main()
 
 if __name__ == '__main__':
--- manage_files.py
+++ manage_files.py
@@ -8,34 +8,36 @@ import main_window
 class DefaultLocations:
     def __init__(self):
         # default values
-        self.file = "default_locations.txt"
-        self.outputdir_initial = "."
-        self.itunesdir = "."
-        self.historyfile = "history.txt"
-        self.genres_file = "genres.txt"
+        self.file = 'default_locations.txt'
         self.defaults_dict = {
-                                "outputdir_initial": "",
-                                "itunesdir": "",
-                                "historyfile": "history.txt",
-                                "genres_file": "genres.txt"
-                            }
-
-
-        self.outputdir_initial = "E:\\zztransfer\\"
-        self.itunesdir = "E:\\Music\\iTunes\\iTunes Media\\Automatically Add to iTunes\\"
-        self.historyfile = "history.txt"
-        self.genres_file = "genres.txt"
+            "outputdir_initial": '.',
+            "itunesdir": '.',
+            "historyfile": 'history.txt',
+            "genres_file": 'genres.txt'
+        }
 
     def read_file(self):
         with open(self.file, 'r') as rf:
-            df_contents = rf.read(json.loads(rf.readlines()))
+            a = rf.read()
+            df_contents = json.loads(a)
         return df_contents
 
     def check_defaults_file(self):
         if not Path(self.file).is_file():
             self.create_file()
+            return self.read_file()
         else:
-            self.read_file()
+            return self.read_file()
+
+    def create_file(self):
+        json_encode = json.dumps(self.defaults_dict)
+        with open(self.file, 'w') as fp:
+            fp.write(json_encode)
+
+    def load_vars(self):
+        with open(self.file, 'r') as rf:
+            data = json.load(rf)
+        return data
 
 
 class EditGenre:
@@ -50,6 +52,7 @@ class EditGenre:
     def save_genre(self, lines):
         with open(self.file, "w") as f:
             f.write(lines)
+
 
 class History:
     def __init__(self, file):
@@ -91,7 +94,7 @@ class History:
         if max_num_of_lines == 0:
             # return all lines
             with open(self.file, "r") as f:
-                all_lines =  f.readlines()
+                all_lines = f.readlines()
                 return ''.join(all_lines)
         else:
             with open(self.file, "r") as f:
